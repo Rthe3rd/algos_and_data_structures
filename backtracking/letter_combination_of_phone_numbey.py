169075608253c6e1fcf4ee163d0084027d1a5636
Alex Zuniga <alexander.zuniga@fitmentgroup.com>
2024-10-21 08:51:18 -0500

    sorted vowels, phone number combinatios, palindroms

--- backtracking/letter_combination_of_phone_numbey.py
+++ backtracking/letter_combination_of_phone_numbey.py
@@ -13,9 +13,9 @@ def letterCombinations(self, digits: str) -> list[str]:
         print(f'combinations after: {combinations}')
         print(f'===================================')
 
-    return combinations
+    # return combinations
 
-print(letterCombinations('holder', '23'))
+# print(letterCombinations('holder', '23'))
 
 class Solution(object):
     def letterCombinations(self, digits):
@@ -26,8 +26,8 @@ class Solution(object):
         all_combinations  = []
         KEYBOARD = {
             "2": "abc",
-            "3": "def",
-            "4": "ghi",
+            "3": "d",
+            "4": "g",
             "5": "jkl",
             "6": "mno",
             "7": "pqrs",
@@ -37,14 +37,29 @@ class Solution(object):
         def dfs_helper(start_index, path):
             if start_index == len(digits) :
                 all_combinations.append("".join(path))
+                print(f"JUST ADDED THE PATH: {path} to the final result, return to after the dfs call!")
                 return
 
             next_number = digits[start_index]
+            print(f"==============================================")
+            print(f"next number to iterate through the possible letters of: {next_number}")
+            print(f"looping through letters: {KEYBOARD[next_number]}")
             for letter in KEYBOARD[next_number]:
+                print(f"current next_number: {next_number}")
+                print(f"letter being appended: {letter}")
                 path.append(letter)
+                print(f"current path: {path}")
+                print(f"calling dfs recursively on start_index + 1: {start_index + 1} and path: {path}")
                 dfs_helper(start_index + 1, path)
+                print(f"Popping from: {path}")
                 path.pop()
+                print(f"END OF FOR LOOP")
+                print(f"==============================================")
         if not digits:
             return []
         dfs_helper(0, [])
         return all_combinations
+    
+solution = Solution()
+
+print(solution.letterCombinations("34"))
