d1733a509f10de4dde6f4d5bd24e468ed71ff7fe
Alex Zuniga <alexander.zuniga@fitmentgroup.com>
2024-11-08 21:48:05 -0600

    11.8

--- dynamic_programming/num_decodings.py
+++ dynamic_programming/num_decodings.py
@@ -12,49 +12,52 @@
 
 def numDecodings(digits):
 
-  mem = dict()
+    mem = dict()
 
-  def dfs(start_index):
-      if mem.get(start_index):
-          return mem[start_index]
+    # DFS with start index
+    def dfs(start_index):
+        if mem.get(start_index):
+            return mem[start_index]
 
-      if start_index == len(digits):
-          return 1
+        if start_index == len(digits):
+            return 1
+        # ans = initial value
+        ways = 0
 
-      ways = 0
+        # Similar to generate valid paranthesis, the get edges is made with if statements and conditionals
+        if digits[start_index] == "0":
+            return ways
 
+        # ans = aggretation with updated parameters. Here we don't need to pass state
+        ways += dfs(start_index + 1)
+        # Similar to generate valid paranthesis, the get edges is made with if statements and conditionals
+        if 10 <= int(digits[start_index: start_index + 2]) <= 26:
+            ways += dfs(start_index + 2)
 
-      if digits[start_index] == "0":
-          return ways
+        mem[start_index] = ways
+        return ways
 
-      ways += dfs(start_index + 1)
-      if 10 <= int(digits[start_index: start_index + 2]) <= 26:
-          ways += dfs(start_index + 2)
-
-      mem[start_index] = ways
-      return ways
-
-  return dfs(0)
-
-
-
-s1 = "12"
-# Output: 2
-# # Explanation:
-# "12" could be decoded as "AB" (1 2) or "L" (12).
-
-s2 = "226"
-# Output: 3
-# Explanation:
-# "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+    return dfs(0)
 
 
-s3 = "06"
-# Output: 0
-# Explanation:
-# "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06"). In this case, the string is not a valid encoding, so return 0.
+
+    s1 = "12"
+    # Output: 2
+    # # Explanation:
+    # "12" could be decoded as "AB" (1 2) or "L" (12).
+
+    s2 = "226"
+    # Output: 3
+    # Explanation:
+    # "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 
 
-print(numDecodings(s1))
-print(numDecodings(s2))
-print(numDecodings(s3))
+    s3 = "06"
+    # Output: 0
+    # Explanation:
+    # "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06"). In this case, the string is not a valid encoding, so return 0.
+
+
+    print(numDecodings(s1))
+    print(numDecodings(s2))
+    print(numDecodings(s3))
